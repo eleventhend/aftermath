7637c695aa6c8af24b7067b67571d3c78c5b4281
Jeff Ferguson <eleventhend@gmail.com>
2015-08-17 10:44:09 -0400

    moved variables to command line arguments

--- aftermath.py
+++ aftermath.py
@@ -33,35 +33,40 @@ def encounter(maneuver, pool, die, success):
         success += 1
     return success
 
-#command line arguments
+"""
+command line arguments
+    Currently only sets dice pips
+"""
 parser = argparse.ArgumentParser(
     description='Run Monte Carlo simulations for Aftermath Engine', 
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("--pips", type=int, default=6,
     help="Optional: set the dice pips for the dice in pool/maneuver")
+parser.add_argument("--rerolls", type=int, default=0,
+    help="Optional: set the number of rerolls allowed for the player's pool")
+parser.add_argument("--n", type=int, default=10000,
+    help="Optional: set the number of samples for the Monte Carlo simulation")
 args = parser.parse_args()
-
-#Set dice pips (default 6)
 die = args.pips
+rerolls = args.rerolls
+sample = args.n
 
 """
 die_type should be a hierarchical index on one side of color
     (not clear by mechanics which side yet)
 
+Create dice pool & populate it with available dice (=player default dice pool)
 'count' - the number of dice available in pool
 'required' - the number of dice of that pip required for a maneuver
 """
-
-#Create dice pool & populate it with available dice (=player default dice pool)
 pool = pd.DataFrame(index=('combat', 'social', 'skill', 'wild'), 
     columns=('count',))
 pool['count'] = (4, 1, 3, 0)
-
-
 d_type = ('combat', 'social', 'skill')
 d_pips = range(1, die+1)
 midx = pd.MultiIndex.from_tuples(list(itertools.product(d_type, d_pips)))
-default_maneuver = pd.DataFrame(0, index=midx, columns=('required','count','result'))
+default_maneuver = pd.DataFrame(0, index=midx, 
+    columns=('required','count','result'))
 
 inertial_purge = default_maneuver
 inertial_purge['required'].loc['combat', 2] = 0
@@ -70,16 +75,13 @@ inertial_purge['required'].loc['skill', 6] = 2
 
 #init success as 0 in decimal form
 success = 0.0
-#set rerolls allowed by engine
-rerolls = 0
-#set number of samples to be run
-sample = 10000
-
 #run samples
 for x in xrange(0, sample):
     inertial_purge['count'] = 0
     inertial_purge['result'] = 0
     success = encounter(inertial_purge, pool, die, success)
 
-print "%(y)s/%(s)s succeeded after %(r)s rerolls" %{"y": int(success), "s": sample, "r": rerolls}
-print "%(p)s%% success rate after %(r)s rerolls" %{"p": ((success/sample)*100), "r": rerolls}
+print ("%(y)s/%(s)s succeeded after %(r)s rerolls" 
+    %{"y": int(success), "s": sample, "r": rerolls})
+print ("%(p)s%% success rate after %(r)s rerolls" 
+    %{"p": ((success/sample)*100), "r": rerolls})
